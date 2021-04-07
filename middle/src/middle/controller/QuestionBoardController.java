e846ccd2537612fd1dac36c207178ef15ea48bbc
yuchan hong <coocieco1@gmail.com>
2021-04-07 16:22:20 +0900

    기본세팅 ㄴ server.xml- JNDI 세팅, controller 커맨드가져오기 콘솔에서 정상확인

---
+++ middle/src/middle/controller/QuestionBoardController.java
@@ -0,0 +1,36 @@
+package middle.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/questionBoard/*")
+public class QuestionBoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public QuestionBoardController() {
+    }
+ 
+    public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String requestURI = request.getRequestURI();
+    		System.out.println("requestURI: "+requestURI);
+    	String contextPath = request.getContextPath();
+    		System.out.println("contextPath: "+contextPath);
+    	String command = requestURI.substring(contextPath.length()+7);
+    		System.out.println("command: "+command);
+    		
+    		
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
